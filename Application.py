37fab51a804a181452b238c7ec8257fcb18adb07
gajaen <gajaen@icloud.com>
2020-07-06 16:24:22 +0200

    DB connection

--- Application.py
+++ Application.py
@@ -9,11 +9,20 @@ test = ""
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
+
 DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("DB_NAME")
 DB_USER = os.environ.get("DB_USER")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
+
+print(DB_HOST)
+print(DB_PORT)
+print(DB_NAME)
+print(DB_USER)
+print(DB_PASSWORD)
+
+
 
 app=Flask(__name__)
 @app.route("/")
