0ee1f50e788f5ca867f3ce5d939e942abeafab9e
gajaen <gajaen@icloud.com>
2020-07-07 14:03:17 +0200

    database abstraction

--- Application.py
+++ Application.py
@@ -1,14 +1,38 @@
 from flask import Flask
-
+import exception_handling
 import database
+from flask import render_template
 
-app=Flask(__name__)
+app=Flask(__name__, template_folder='exception_handling/templates', static_folder='exception_handling/static')
+
 @app.route("/")
-def hello():
-    return database.get_lost("42f8f207-c09a-4b03-8281-726a73b80094").to_json()
+def root():
+    return database.get_all_lost().to_json()
+
+@app.route("/get_score")
+def get_score():
+    database.myConnection.rollback()
+    return database.get_lost("42f8f207-c09a-4b03-8281-726a73b8094").to_json()
 
 @app.route("/name")
-def hello2():
-    return "route funker"
+def insert_to_database():
+    database.insert_match_table("1", "2", "2")
+    return "inserted"
+
+@app.errorhandler(400)
+def handle_bad_request(error):
+    data = {'status': 'error', 'errormessage ': error}
+    return render_template('404.html', error=error), 404, data
+
+@app.errorhandler(404)
+def not_found_error(error):
+    data = {'status': 'error', 'errormessage ': error}
+    return render_template('404.html', error=error), 404, data
+
+@app.errorhandler(500)
+def internal_error(error):
+    data = {'status': 'error', 'errormessage ': error}
+    return render_template('500.html', error=error), 500, data
+
 
 
--- database.py
+++ database.py
@@ -14,18 +14,33 @@ database = os.environ.get("DB_NAME")
 myConnection = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
 
 
+def read_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute( query )
+        names = [ x[0] for x in cursor.description]
+        rows = cursor.fetchall()
+        return pd.DataFrame( rows, columns=names)
+    finally:
+        if cursor is not None:
+            cursor.close()
+
+
 def get_all_lost():
-    df_all_lost = pd.read_sql_query("select * from lost;", myConnection)
-    return df_all_lost
+    return read_query(myConnection, "select * from lost;")
+
 
 def get_all_found():
-    df_all_found = pd.read_sql_query("select * from found;", myConnection)
-    return df_all_found
+    return read_query(myConnection, "select * from found;")
 
-def get_lost(lostID):
-    df_lost = pd.read_sql_query("select * from lost where refnr = '"+lostID+"';",  myConnection)
-    return df_lost
 
-def get_found(foundID):
-    df_found = pd.read_sql_query("select * from found where refnr = '"+foundID+"';",  myConnection)
-    return df_found
+def get_all_found(lostID):
+    return read_query(myConnection, "select * from lost where lostid = '"+lostID+"';")
+
+
+def get_all_lost(foundID):
+    return read_query(myConnection, "select * from found where foundid = '"+foundID+"';")
+
+
+def insert_match_table(score, lostID, foundID):
+    return read_query(myConnection, "INSERT INTO public.match( score, lostID, foundID) VALUES ('"+score+"','"+lostID+"','"+foundID+"');")
