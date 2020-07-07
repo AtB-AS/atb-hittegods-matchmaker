51e0c9603cbb48ceb9ecfd6b95363f4029f45e12
gajaen <gajaen@icloud.com>
2020-07-07 09:38:54 +0200

    database and queries file

--- Application.py
+++ Application.py
@@ -1,60 +1,14 @@
 from flask import Flask
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
-import psycopg2
-import Mathing
-import pandas as pd
-import sqlite3
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-
-DB_HOST = os.environ.get("DB_HOST")
-DB_PORT = os.environ.get("DB_PORT")
-DB_NAME = os.environ.get("DB_NAME")
-DB_USER = os.environ.get("DB_USER")
-DB_PASSWORD = os.environ.get("DB_PASSWORD")
+import database
 
 app=Flask(__name__)
 @app.route("/")
 def hello():
-    pd.reads
-    return (str(Mathing.testMatching()))
+    return database.get_from_dataframe().to_json()
 
 @app.route("/name")
 def hello2():
-    return print((get_from_dataframe()))
+    return "test"
 
-hostname = os.environ.get("DB_HOST")
-username = os.environ.get("DB_USER")
-password = os.environ.get("DB_PASSWORD")
-database = os.environ.get("DB_NAME")
 
-# Simple routine to run a query on a database and print the results:
-def doQuery( conn ) :
-    cur = conn.cursor()
-
-    cur.execute( "SELECT * from lost" )
-
-    result = cur.fetchall()
-    for row in result:
-        print(row[0])
-    return row
-
-def get_names():
-    myConnection = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
-    query = doQuery( myConnection )
-    df = pd.read_sql_query("select * from lost;", myConnection)
-    myConnection.close()
-    return query
-
-def get_from_dataframe():
-    myConnection = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
-    query = doQuery(myConnection)
-    df = pd.read_sql_query("select * from lost;", myConnection)
-    print("****** df *****")
-    return df
-    print(df)
-
-get_from_dataframe()
--- database.py
+++ database.py
@@ -1,17 +1,19 @@
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
+import psycopg2
+import pandas as pd
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
-
-DB_HOST = os.environ.get("DB_HOST")
-DB_PORT = os.environ.get("DB_PORT")
-DB_NAME = os.environ.get("DB_NAME")
-DB_USER = os.environ.get("DB_USER")
-DB_PASSWORD = os.environ.get("DB_PASSWORD")
 
 hostname = os.environ.get("DB_HOST")
 username = os.environ.get("DB_USER")
 password = os.environ.get("DB_PASSWORD")
 database = os.environ.get("DB_NAME")
+
+def get_from_dataframe():
+    myConnection = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
+    df = pd.read_sql_query("select * from lost;", myConnection)
+    print("****** df *****")
+    return df
