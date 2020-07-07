f10e195153df73c8e9a58b9d6d1d4d930733b4b4
GauteHolen <gaute.ah@gmail.com>
2020-07-07 14:09:58 +0200

    mvp read from db and does matching

--- Application.py
+++ Application.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 import database
+import Mathing
 
 app=Flask(__name__)
 @app.route("/")
@@ -11,4 +12,18 @@ def hello():
 def hello2():
     return "route funker"
 
+@app.route("/matching")
+def matchingFromDB():
 
+    allLost = database.get_all_lost()
+    lost=database.get_lost("42f8f207-c09a-4b03-8281-726a73b80094")
+
+    x=lost
+    data=allLost
+
+    print(list(data.columns))
+    print("length of x :" + str(len(x)))
+
+    bestMatches=Mathing.doMatching(x,data,5)
+
+    return str(list(bestMatches))
