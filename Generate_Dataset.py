db2c85b1e241cadf51362ab366d3aa30d1923b3a
gajaen <gajaen@icloud.com>
2020-07-08 10:43:40 +0200

    hard reset yesterday

--- Generate_Dataset.py
+++ Generate_Dataset.py
@@ -11,80 +11,51 @@ import matplotlib.pyplot as plt
 import random
 
 
-
-
-
 def getDataFrame(fileLocation):
-    datafile=open(fileLocation,'r')
-    df = pd.read_csv(datafile,sep='\t',skiprows=(0),header=(0))
+    datafile = open(fileLocation, "r")
+    df = pd.read_csv(datafile, sep="\t", skiprows=(0), header=(0))
     return df
 
+
 def generateRandomDataPoint(df):
-    
-    randomValues=[]
-    columnNames=list(df.columns)
-    
-    
-    
+
+    randomValues = []
+    columnNames = list(df.columns)
+
     for colName in columnNames:
-        values=df[colName].tolist()
-        values=removeEmptyFromList(values)
-        randomValue=generateRandomValue(values)
+        values = df[colName].tolist()
+        values = removeEmptyFromList(values)
+        randomValue = generateRandomValue(values)
         randomValues.append(randomValue)
-    
-      
+
     return randomValues
 
+
 def generateRandomValue(values):
-    max=len(values)
-    randomIndex=random.randint(0,max-1)
+    max = len(values)
+    randomIndex = random.randint(0, max - 1)
     return values[randomIndex]
 
+
 def removeEmptyFromList(List):
-    newList=[]
+    newList = []
     for item in List:
-        if(pd.isna([item])==False):
+        if pd.isna([item]) == False:
             newList.append(item)
     return newList
 
 
-def generateRandomDF(df,n):
-    columnNames=list(df.columns)
-    newDFDict={}
+def generateRandomDF(df, n):
+    columnNames = list(df.columns)
+    newDFDict = {}
     for i in range(n):
-        newDFDict.update({str(i):generateRandomDataPoint(df)})
-    newDF=pd.DataFrame.from_dict(newDFDict, orient='index', columns=columnNames)
+        newDFDict.update({str(i): generateRandomDataPoint(df)})
+    newDF = pd.DataFrame.from_dict(newDFDict, orient="index", columns=columnNames)
     return newDF
 
-def generateDataSet(inputFile, outputFile,n):
+
+def generateDataSet(inputFile, outputFile, n):
     columnNameValues = getDataFrame(inputFile)
 
-    dataSet=generateRandomDF(columnNameValues,n)
-    dataSet.to_csv('Data/'+outputFile,sep='\t',index_label='ref')
-
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-
-
-
-    
-    
+    dataSet = generateRandomDF(columnNameValues, n)
+    dataSet.to_csv("Data/" + outputFile, sep="\t", index_label="ref")
--- MatchingFromDB.py
+++ MatchingFromDB.py
@@ -1,23 +1,23 @@
 import database
 import Mathing
 
-def matchingDB(x_type,x_id):
 
-    if(x_type=='found'):
+def matchingDB(x_type, x_id):
+
+    if x_type == "found":
         data = database.get_all_lost()
-        x=database.get_found(x_id)
-    elif(x_type=='lost'):
+        x = database.get_found(x_id)
+    elif x_type == "lost":
         data = database.get_all_found()
         x = database.get_lost(x_id)
 
-
-    bestMatches=Mathing.doMatching(x,data,5)
+    bestMatches = Mathing.doMatching(x, data, 5)
 
     for match in bestMatches:
-        values=list(match.values())
-        if(x_type=='found'):
-            database.insert_match_table(values[2],values[1],values[0])
-        elif(x_type=='lost'):
-            database.insert_match_table(values[2],values[0],values[1])
+        values = list(match.values())
+        if x_type == "found":
+            database.insert_match_table(values[2], values[1], values[0])
+        elif x_type == "lost":
+            database.insert_match_table(values[2], values[0], values[1])
 
     return str(list(bestMatches))
