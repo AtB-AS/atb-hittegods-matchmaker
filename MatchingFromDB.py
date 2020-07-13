c20a13792534c86e5a4e819cf94cc40b1248b28f
GauteHolen <gaute.ah@gmail.com>
2020-07-13 18:05:00 +0200

    threshold for score insert to DB at 0.7

--- MatchingFromDB.py
+++ MatchingFromDB.py
@@ -21,10 +21,13 @@ def matchingDB(x_type, x_id):
     for match in bestMatches:
         values = list(match.values())
         print("**** looop *****")
-        if x_type == "found":
-            database.insert_match_table(values[2], values[1], values[0])
-        elif x_type == "lost":
-            database.insert_match_table(values[2], values[0], values[1])
+        if(values[2]>0.7):
+            if x_type == "found":
+                database.insert_match_table(values[2], values[1], values[0])
+            elif x_type == "lost":
+                database.insert_match_table(values[2], values[0], values[1])
+        else:
+            print("Score too low, not sent to DB")
 
     print(str(list(bestMatches)))
     return str(list(bestMatches))
